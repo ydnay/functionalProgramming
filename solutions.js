a4d96ab69ed2d87aec583fadce9dddbd8bb691b6
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-11-14 08:53:54 -0500

    filter arr using forEach

--- solutions.js
+++ solutions.js
@@ -46,7 +46,7 @@ const newReleases = [
     "title": "Die Hard",
     "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
     "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-    "rating": [4.0],
+    "rating": 4.0,
     "bookmark": []
   },
   {
@@ -54,7 +54,7 @@ const newReleases = [
     "title": "Bad Boys",
     "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
     "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-    "rating": [5.0],
+    "rating": 5.0,
     "bookmark": [{ id: 432534, time: 65876586 }]
   },
   {
@@ -62,7 +62,7 @@ const newReleases = [
     "title": "The Chamber",
     "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
     "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-    "rating": [4.0],
+    "rating": 4.0,
     "bookmark": []
   },
   {
@@ -70,7 +70,7 @@ const newReleases = [
     "title": "Fracture",
     "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
     "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-    "rating": [5.0],
+    "rating": 5.0,
     "bookmark": [{ id: 432534, time: 65876586 }]
   }
 ]
@@ -103,4 +103,26 @@ Array.prototype.myMap = function(f) {
 
 const projectArr1 = arr => arr.map(elem => ({id: elem.id, title: elem.title}));
 
-console.log(projectArr1(newReleases));
+// console.log(projectArr1(newReleases));
+
+// Filtering Arrays
+// Like projection, filtering an array is also a very common operation. To filter an array we apply a test to each 
+// item in the array and collect the items that pass into a new array.
+
+// Exercise 6: Use forEach() to collect only those videos with a rating of 5.0
+// Use forEach() to loop through the videos in the newReleases array and, if a video has a rating of 5.0, add it to 
+// the videos array.
+
+const collectElem = arr => {
+  const res = [];
+  arr.forEach(elem => {
+    if (elem.rating === 5) {
+      res.push(elem);
+    }
+  })
+
+  return res;
+}
+
+console.log(collectElem(newReleases));
+
