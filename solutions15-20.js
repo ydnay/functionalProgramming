3e4fb23a36788d55d7f8953676b9e71ff47e2da0
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-12-12 12:33:48 -0500

    rename files

--- solutions15-20.js
+++ solutions15-20.js
@@ -143,3 +143,110 @@ const reduceWithIniVal = arr => arr.
   }, {});
 
 // console.log(reduceWithIniVal(videos));
+
+//  movieList
+const movieLists = [
+  {
+    name: "New Releases",
+    videos: [
+      {
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxarts": [
+          { width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+          { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+        ],
+        "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 4.0,
+        "bookmark": []
+      },
+      {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxarts": [
+          { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+          { width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+
+        ],
+        "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 5.0,
+        "bookmark": [{ id:432534, time:65876586 }]
+      }
+    ]
+  },
+  {
+    name: "Thrillers",
+    videos: [
+      {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxarts": [
+          { width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+          { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+        ],
+        "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 4.0,
+        "bookmark": []
+      },
+      {
+        "id": 675465,
+        "title": "Fracture",
+        "boxarts": [
+          { width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+          { width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+          { width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+        ],
+        "url": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": 5.0,
+        "bookmark": [{ id:432534, time:65876586 }]
+      }
+    ]
+  }
+];
+
+// Exercise 20: Retrieve the id, title, and smallest box art url for every video.
+// This is a variation of the problem we solved earlier, where we retrieved the url 
+// of the boxart with a width of 150px. This time we'll use reduce() instead of 
+// filter() to retrieve the smallest box art in the boxarts array.
+
+Array.prototype.concatMap = function(projectionFunctionThatReturnsArray) {
+  return this.
+    map(function(item) {
+      return projectionFunctionThatReturnsArray(item);
+    }).
+    myConcatAll();
+};
+
+Array.prototype.myConcatAll = function(arr) {
+  const res = [];
+  this.forEach(nestedArr => nestedArr.forEach(elem => res.push(elem)));
+
+  return res;
+}
+
+// Use one or more concatMap, map, and reduce calls to create an array with the following items 
+// (order doesn't matter)
+// [
+// {"id": 675465,"title": "Fracture","boxart":"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+// {"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+// {"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" },
+// {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+// ];
+
+const retrieveSmallest = arr => arr.
+  concatMap(movieList =>
+    movieList.videos.concatMap(video =>
+      video.boxarts.
+        myReduce((acc, cur) => 
+          acc.width * acc.height < cur.width * cur.height ? acc : cur).
+        map(boxart =>
+          ({
+            id: video.id,
+            title: video.title,
+            boxart: boxart.url
+          })
+        )
+      )
+    )
+
+console.log(retrieveSmallest(movieLists));
